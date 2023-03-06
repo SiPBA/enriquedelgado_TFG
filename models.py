ef1ef237652d23f9fea29f23f7c810bde6161ab2
Enrique Delgado <enriquedelgadodelasheras@gmail.com>
2023-03-06 21:19:01 +0100

    Añadida capa convolucional en el decoder del CVAE

--- models.py
+++ models.py
@@ -108,20 +108,12 @@ class CVAE_3D(nn.Module):
             nn.ReLU(True),
             nn.ConvTranspose3d(16, 8, 3, stride=2, padding=1, output_padding=1),
             nn.BatchNorm3d(8),
-            nn.ReLU(True),
-            # En la capa de abajo se almacena en un canal la media y en el otro el logvar de cada pixel
+            nn.ReLU(True),            
             nn.ConvTranspose3d(8, 2, 3, stride=2, padding=1, output_padding=1),
+            # En la capa de abajo se almacena en un canal la media y en el otro el logvar de cada pixel
+            nn.Conv3d(2,2,3, stride=1, padding=1)
         )
 
-        # Capas convolucionales para computar la media y el logaritmo de la varianza de los pixeles de la imagen recontruida
-        # self.cv1 = nn.Sequential(
-        #     nn.Conv3d(1, 8, 3, stride=2, padding=1),
-        #     nn.ConvTranspose3d(8, 1, 3, stride=2, padding=1, output_padding=1)
-        # )
-        # self.cv2 = nn.Sequential(
-        #     nn.Conv3d(1, 8, 3, stride=2, padding=1),
-        #     nn.ConvTranspose3d(8, 1, 3, stride=2, padding=1, output_padding=1)
-        # )
 
     def sample(self, mu, logvar):
         var = torch.exp(0.5 * logvar)
@@ -143,8 +135,6 @@ class CVAE_3D(nn.Module):
 
     def decode(self, z):
         x = self.decoder_CVAE(z)
-        #mu_x = self.cv1(x)
-        #logvar_x = self.cv2(x)
         mu_x = x[:,0,:,:,:]
         logvar_x = x[:,1,:,:,:]
         return mu_x, logvar_x
